7939765de241480a6731ed9f5f3f9d379bbfef5b
Scott Ricketts <SJR@imac.lan>
2020-08-14 16:50:40 -0500

    import Flask, begin routes.

---
+++ app.py
@@ -0,0 +1,29 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/ api/v1.0/<start><br/>"
+        f"/ api/v1.0/<start>/<end><br/>"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
